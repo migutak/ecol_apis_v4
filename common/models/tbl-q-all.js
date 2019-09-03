b50461234be14ed79b86f10d8730eee10d1bb9ea
migutak <migutak@gmail.com>
2019-09-03 15:23:29 +0300

    more with plan

--- common/models/tbl-q-all.js
+++ common/models/tbl-q-all.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = function (Tblqall) {
-    Tblqall.viewallcc = function (pagesize,pagenum, cb) {
-        
+    Tblqall.viewallcc = function (pagesize, pagenum, cb) {
+
         let start = pagesize * pagenum;
-        
+
         var ds = Tblqall.dataSource;
         var countviewall = "select count(*) total from cards_stage";
-        var viewallq = "select * from cards_stage offset "+start+" rows fetch next "+pagesize+" rows only";
+        var viewallq = "select * from cards_stage offset " + start + " rows fetch next " + pagesize + " rows only";
         ds.connector.query(countviewall, [], function (err, count) {
             if (err) console.error(err);
             ds.connector.query(viewallq, [], function (error, viewall) {
@@ -48,13 +48,45 @@ module.exports = function (Tblqall) {
         },
     });
 
-    Tblqall.viewall = function (pagesize,pagenum, cb) {
-        
+    Tblqall.cmdacc = function (custnumber, cb) {
+
+        var ds = Tblqall.dataSource;
+        var query = "select * from (select accnumber,custnumber,outsbalance oustbalance from custview_stage union select CARDNUMBER,CARDACCT,to_number(OUTBALANCE) from qcards) where custnumber = " + custnumber;
+
+        ds.connector.query(query, [], function (error, result) {
+            if (error) console.error(error);
+            cb(err, result);
+        })
+    }
+
+    Tblqall.remoteMethod('cmdacc', {
+        accepts: [
+            {
+                arg: 'custnumber',
+                type: 'string',
+                http: {
+                    source: 'query',
+                },
+            }
+        ],
+        returns: {
+            arg: 'result',
+            type: 'array',
+            root: true,
+        },
+        http: {
+            path: '/cmdacc',
+            verb: 'get',
+        },
+    });
+
+    Tblqall.viewall = function (pagesize, pagenum, cb) {
+
         let start = pagesize * pagenum;
-        
+
         var ds = Tblqall.dataSource;
         var countviewall = "select count(*) total from tbl_q_all";
-        var viewallq = "select * from tbl_q_all offset "+start+" rows fetch next "+pagesize+" rows only";
+        var viewallq = "select * from tbl_q_all offset " + start + " rows fetch next " + pagesize + " rows only";
         ds.connector.query(countviewall, [], function (err, count) {
             if (err) console.error(err);
             ds.connector.query(viewallq, [], function (error, viewall) {
